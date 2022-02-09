fbdd70e654da196564d568bef8a9b4a630595004
Vova Stefyuk <vova.stefyuk@gmail.com>
2022-02-09 21:34:51 +0200

    Добавьте файлы проекта.

---
+++ ComparisionOfAngles/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisionOfAngles
+{
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double GetAngleOX()
+        {
+            double AngleOX = Math.Atan2(Y, X) * 180 / Math.PI;
+            if (AngleOX < 0) return 180 + Math.Abs(AngleOX);
+            else return AngleOX;
+        }
+
+        public bool IsMoreAngleOX(Point point)
+        {
+            return GetAngleOX() > point.GetAngleOX();
+        }
+
+        public bool IsLessAngleOX(Point point)
+        {
+            return GetAngleOX() < point.GetAngleOX();
+        }
+        public bool IsEqualAngleOX(Point point)
+        {
+            return GetAngleOX() == point.GetAngleOX();
+        }
+
+        public void PrintPoint()
+        {
+            Console.Write($"x = {X}; y = {Y}");
+        }
+
+    }
+}
---
+++ ComparisionOfAngles/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ComparisionOfAngles
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            bool complete = false;
+            while (!complete)
+            {
+                Console.WriteLine("Enter the coordinates of the points " +
+                    "\n data format: x1,y1,x2,y2");
+                string enterValue = Console.ReadLine();
+                string[] splitEnterValue = enterValue.Split(new char[] { ',' });
+                Point A;
+                Point B;
+                try
+                {
+                    SetPoints(out A, out B, splitEnterValue);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrectly entered data");
+                    continue;
+                }
+                if (A.IsMoreAngleOX(B)) Console.WriteLine("Larger angle forms a segment: OA");
+                else if (A.IsLessAngleOX(B)) Console.WriteLine("Larger angle forms a segment: OB");
+                else Console.WriteLine("The  segments form the same angles");
+
+                Console.WriteLine("Check other points?");
+                if (Console.ReadLine() == "yes") continue;
+                else break;
+            }
+        }
+        static void SetPoints(out Point a, out Point b, string[] value)
+        {
+            if (value.Length != 4) throw new ArgumentException();
+            try
+            {
+                a = new Point(Convert.ToDouble(value[0]), Convert.ToDouble(value[1]));
+                b = new Point(Convert.ToDouble(value[2]), Convert.ToDouble(value[3]));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
